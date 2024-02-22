dc4a887a1b4397633b6579899514d0403dd64c4f
Mkien <manhkien1005@gmail.com>
2024-02-22 11:17:05 +0700

    xóa import không cần thiết 2

--- src/main/java/controller/indexForward.java
+++ src/main/java/controller/indexForward.java
@@ -1,12 +1,10 @@
 package controller;
 
 import java.io.IOException;
-import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
