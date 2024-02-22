576f703f8cb081d85ef214bda4f727c597810f1c
Mkien <manhkien1005@gmail.com>
2024-02-22 12:53:22 +0700

    Update dbConnection.java

--- src/main/java/db/dbConnection.java
+++ src/main/java/db/dbConnection.java
@@ -1,17 +1,14 @@
 package db;
+
 import java.sql.*;
+
 public class dbConnection {
 	public static Connection createConnection() {
 		Connection connection = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/kingoffruit","root","");
-			if (connection != null) {
-                System.out.println("Connected to the database successfully");
-            } else {
-                System.out.println("Failed to connect to the database");
-            }
-		}catch(SQLException e) {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/kingoffruit", "root", "");
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
